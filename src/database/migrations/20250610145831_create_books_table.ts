4f3206186ba0d10b05b99d45a13851e790fbb32c
Ivan <contaivanrodrigues@gmail.com>
2025-06-10 12:02:13 -0300

    feat: add table for authors

--- src/database/migrations/20250610145831_create_books_table.ts
+++ src/database/migrations/20250610145831_create_books_table.ts
@@ -3,13 +3,17 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("books", (table) => {
     table.increments("id").primary();
-    table.string("title").notNullable();
-    table.string("author").notNullable();
+    table.string("title").notNullable().unique();
+    table.integer("authors_id").unsigned().notNullable()
+      .references("id")
+      .inTable("authors")
+      .onDelete("CASCADE");
     table.integer("year").notNullable();
     table.timestamps(true, true);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable("books");
+  return knex.schema.dropTableIfExists("books");
 }
+
